dd1a528dbf470f6c5a5394aa9289d8292d8603a5
Nick Bild <nick.bild@gmail.com>
2021-07-04 15:57:08 -0400

    Add speech script.

---
+++ speech.py
@@ -0,0 +1,260 @@
+from gi_sound import GiSound
+import time
+
+
+def main():
+    sound = GiSound(8, 10, 12, 16, 18, 22, 24, 26, 36, 38, 40)
+
+    sound.set_volume(1, 1, 1)
+    sound.set_mixer(1, 1, 1, 1, 1 ,1)
+    sound.set_tone(2600, 2600, 2600)
+    sound.set_noise(31)
+    
+    time.sleep(0.030)
+
+    sound.volume_off()
+    time.sleep(0.030)
+
+    sound.set_tone(4058, 4058, 4058)
+    sound.set_volume(1, 1, 1)
+    time.sleep(0.030)
+
+    sound.set_tone(4023, 4023, 4023)
+    sound.set_noise(24)
+    time.sleep(0.030)
+
+    sound.set_tone(74, 74, 74)
+    sound.set_noise(25)
+    time.sleep(0.030)
+
+    sound.set_tone(864, 864, 864)
+    sound.set_noise(18)
+    time.sleep(0.030)
+
+    sound.set_tone(190, 190, 190)
+    sound.set_noise(19)
+    time.sleep(0.030)
+
+    sound.set_tone(510, 510, 510)
+    sound.set_noise(18)
+    time.sleep(0.030)
+
+    sound.set_tone(732, 732, 732)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(299, 299, 299)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(3796, 3796, 3796)
+    sound.set_noise(31)
+    time.sleep(0.030)
+    
+    sound.set_tone(2159, 2159, 2159)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(392, 392, 392)
+    sound.set_noise(12)
+    time.sleep(0.030)
+
+    sound.set_tone(299, 299, 299)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(395, 395, 395)
+    sound.set_noise(18)
+    time.sleep(0.030)
+
+    sound.set_tone(1537, 1537, 1537)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(1205, 1205, 1205)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(925, 925, 925)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(1115, 1115, 1115)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(990, 990, 990)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(3722, 3722, 3722)
+    sound.set_noise(29)
+    time.sleep(0.030)
+
+    sound.volume_off()
+    time.sleep(0.030)
+
+    sound.set_tone(6, 6, 6)
+    sound.set_noise(0)
+    sound.set_volume(1, 1, 1)
+    time.sleep(0.030)
+
+    sound.set_tone(2140, 2140, 2140)
+    sound.set_noise(31)
+    time.sleep(0.030)
+
+    sound.set_tone(859, 859, 859)
+    sound.set_noise(26)
+    time.sleep(0.030)
+
+    sound.set_tone(230, 230, 230)
+    sound.set_noise(30)
+    time.sleep(0.030)
+
+    sound.set_tone(185, 185, 185)
+    sound.set_noise(30)
+    time.sleep(0.030)
+
+    sound.set_tone(273, 273, 273)
+    sound.set_noise(25)
+    time.sleep(0.060)
+
+    sound.set_tone(346, 346, 346)
+    sound.set_noise(28)
+    time.sleep(0.030)
+
+    sound.set_tone(1340, 1340, 1340)
+    sound.set_noise(30)
+    time.sleep(0.030)
+
+    sound.set_tone(296, 296, 296)
+    sound.set_noise(31)
+    time.sleep(0.030)
+
+    sound.set_tone(1448, 1448, 1448)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(1444, 1444, 1444)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(1572, 1572, 1572)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(2159, 2159, 2159)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(2846, 2846, 2846)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(2944, 2944, 2944)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(859, 859, 859)
+    sound.set_noise(26)
+    time.sleep(0.030)
+
+    sound.set_tone(299, 299, 299)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(2985, 2985, 2985)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(354, 354, 354)
+    sound.set_noise(6)
+    time.sleep(0.090)
+
+    sound.set_tone(261, 261, 261)
+    sound.set_noise(18)
+    time.sleep(0.030)
+
+    sound.set_tone(73, 73, 73)
+    sound.set_noise(31)
+    time.sleep(0.030)
+
+    sound.set_tone(205, 205, 205)
+    sound.set_noise(31)
+    time.sleep(0.030)
+
+    sound.set_tone(2796, 2796, 2796)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(270, 270, 270)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(142, 142, 142)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(2918, 2918, 2918)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(1436, 1436, 1436)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(256, 256, 256)
+    sound.set_noise(31)
+    time.sleep(0.060)
+
+    sound.set_tone(185, 185, 185)
+    sound.set_noise(30)
+    time.sleep(0.030)
+
+    sound.set_tone(256, 256, 256)
+    sound.set_noise(31)
+    time.sleep(0.030)
+
+    sound.set_tone(3505, 3505, 3505)
+    sound.set_noise(18)
+    time.sleep(0.030)
+
+    sound.set_tone(6, 6, 6)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.set_tone(3911, 3911, 3911)
+    sound.set_noise(31)
+    time.sleep(0.030)
+
+    sound.set_tone(3860, 3860, 3860)
+    sound.set_noise(7)
+    time.sleep(0.030)
+
+    sound.set_tone(469, 469, 469)
+    sound.set_noise(18)
+    time.sleep(0.030)
+
+    sound.set_tone(299, 299, 299)
+    sound.set_noise(6)
+    time.sleep(0.030)
+
+    sound.set_tone(189, 189, 189)
+    sound.set_noise(17)
+    time.sleep(0.030)
+
+    sound.set_tone(1537, 1537, 1537)
+    sound.set_noise(0)
+    time.sleep(0.060)
+
+    sound.set_tone(1192, 1192, 1192)
+    sound.set_noise(0)
+    time.sleep(0.030)
+
+    sound.volume_off()
+
+    return
+
+
+if __name__ == "__main__":
+    main()
